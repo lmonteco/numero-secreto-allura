2ccf04ed7673a3c5c86be381d8f7a7e78ce20a79
lucas.monteiro <lmontecosta@gmail.com>
2025-03-13 14:15:21 -0300

    desafio 3

--- Desafio 3/app-d3.js
+++ Desafio 3/app-d3.js
@@ -1,24 +1,7 @@
-let titulo = document.querySelector(`h1`);
-let paginaLabel = document.querySelector(`title`)
-titulo.innerHTML = 'Hora do Desafio';
-paginaLabel.innerHTML = 'Desafio JS'
+// IMC = peso / (altura x altura) - Altura em Metros e Peso em Kilogramas
 
-function exibirMensagemConsole() {
-    console.log('O botão foi clicado');
+function calcularImc(altura,peso) {
+    return indiceMassaCorporal = peso / Math.pow(altura, 2);
 }
 
-function exibirMensagemAlerta() {
-    console.log('Eu amo JS');
-}
-
-function exibirPrompt() {
-    let nomeDaCidade = prompt(`Digite o nome de uma cidade do Brasil:`)
-    alert(`Estive em ${nomeDaCidade} e lembrei de você!`);
-}
-
-function calcularSoma() {
-    numero1 = parseInt(prompt(`Insira um número inteiro:`));
-    numero2 = parseInt(prompt(`Insira outro número inteiro:`));
-    let soma = numero1 + numero2;
-    alert(`${numero1} + ${numero2} = ${soma}`);
-}
+console.log(calcularImc(1.68,94));
