3f7973f12e0f70a41bf284968e00a746c03c6022
lucas.monteiro <lmontecosta@gmail.com>
2025-03-13 13:56:00 -0300

    ajustes desafio 2

--- Desafio 2/app-d2.js
+++ Desafio 2/app-d2.js
@@ -1,30 +1,41 @@
+// Tarefa 1
 function consoleOlaMundo() {
     console.log('Olá Mundo!');
 }
 
+consoleOlaMundo();
+
+// Tarefa 2
 function consoleOlaMundoParametro(nome) {
     console.log(`Olá, ${nome}`);
 }
 
+consoleOlaMundoParametro('Lucas');
+
+// Tarefa 3
 function dobrarNumero(meuNumero) {
     return meuNumero * 2;
 }
 
+console.log(dobrarNumero(3));
+
+// Tarefa 4
 function tirarMedia(meuNumero1,meuNumero2,meuNumero3) {
     return (meuNumero1 + meuNumero2 + meuNumero3)/3;
 }
 
+console.log(tirarMedia(1,2,3));
+
+// Tarefa 5
 function compararMaior(meuNumeroMaior1,meuNumeroMaior2) {
     return Math.max(meuNumeroMaior1,meuNumeroMaior2);
 }
 
+console.log(compararMaior(32,12));
+
+// Tarefa 6
 function numeroMultiplicar(numeroMultiplicado) {
     return numeroMultiplicado * numeroMultiplicado;
 }
 
-consoleOlaMundo();
-consoleOlaMundoParametro('Lucas');
-console.log(dobrarNumero(3));
-console.log(tirarMedia(1,2,3));
-console.log(compararMaior(32,12));
 console.log(numeroMultiplicar(70));
--- Desafio 2/desafio2.md
+++ Desafio 2/desafio2.md
@@ -1,11 +1,6 @@
 [x] - Criar uma função que exibe "Olá, mundo!" no console.
-
 [x] - Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
-
 [x] - Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
-
 [x] - Criar uma função que recebe três números como parâmetros e retorna a média deles.
-
 [x] - Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
-
 [x] - Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
